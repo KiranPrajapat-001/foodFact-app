93cbf19d80adf24bca1650e145b2513766351325
KiranPrajapat-001 <kprajapat73033@gmail.com>
2026-04-01 10:06:06 +0530

    initial: scaffold vite react app

---
+++ src/components/FoodCard.jsx
@@ -0,0 +1,25 @@
+function FoodCard({ product }) {
+  const { product_name, brands, nutriments, image_small_url } = product
+
+  return (
+    <div className="food-card">
+
+      {image_small_url ? (
+        <img src={image_small_url} alt={product_name} />
+      ) : (
+        <p>No Image</p>
+      )}
+
+      <h2>{product_name || "Unknown Product"}</h2>
+      <p>{brands || "Unknown Brand"}</p>
+
+      <p>Calories: {nutriments?.['energy-kcal_100g'] || 'N/A'} kcal</p>
+      <p>Protein: {nutriments?.proteins_100g || 'N/A'} g</p>
+      <p>Carbs: {nutriments?.carbohydrates_100g || 'N/A'} g</p>
+      <p>Fat: {nutriments?.fat_100g || 'N/A'} g</p>
+
+    </div>
+  )
+}
+
+export default FoodCard
